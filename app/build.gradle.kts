fc003c04302a40a4d655d63a732911755f19d25d
Jean-Gabriel Genest <jeangabriel.genest@gmail.com>
2025-03-16 18:56:49 +0100

    Available for android 8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,10 +11,10 @@ android {
 
     defaultConfig {
         applicationId = "fr.jg.sosalert"
-        minSdk = 28
+        minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
