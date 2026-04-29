efc75936c3e97e9019f4f21a43195949c6a93b1c
Jean-Gabriel Genest <jeangabriel.genest@gmail.com>
2026-04-29 15:04:44 +0200

    Upgrade version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "fr.jg.sosalert"
         minSdk = 26
         targetSdk = 37
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
